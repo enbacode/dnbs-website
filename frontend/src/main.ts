fcc74f25bb2d1085dd958b01c9b324286354d113
enbacode <nick.baas94@gmail.com>
2021-04-08 16:22:42 +0200

    initial commit

---
+++ frontend/src/main.ts
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+const test = "12";
+
+window.history.pushState(null, test, null);
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/8EOm4zPHfsY?autoplay=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+        <iframe
+            frameborder="0"
+            scrolling="no"
+            id="chatframe"
+            class="style-scope ytd-live-chat-frame"
+            src="/live_chat?continuation=0ofMyANxGlhDaWtxSndvWVZVTnJkSFkzTlhOd09VdEZXRlYyUmpkMmFtZDRObHAzRWdzNFJVOXROSHBRU0daeldSb1Q2cWpkdVFFTkNnczRSVTl0TkhwUVNHWnpXU0FCMAGCAQIIBIgBAaABi4Pcxeru7wKyAQA%253D"
+        ></iframe>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "Home",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
