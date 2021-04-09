170916ce5f8d428c48ea93da2c916c2f93779705
enbacode <nick.baas94@gmail.com>
2021-04-09 13:48:44 +0200

    ghp

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,9 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dnbs-website/'
-    : '/'
+    : '/',
+    chainWebpack: config => {
+      config.plugins.delete('pwa');
+      config.plugins.delete('workbox');
+    }
 };
